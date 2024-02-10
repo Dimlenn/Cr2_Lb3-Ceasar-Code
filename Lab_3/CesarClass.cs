5e2f868838018a94016f0ec3136a17d724586126
Dimlenn <d_melen@mail.ru>
2024-02-10 15:53:45 +0300

    Add project files.

---
+++ Lab_3/CesarClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    internal class CesarClass
+    {
+        private static Dictionary<char, char> CesarCode;
+        public string Str { get; set; }
+
+        public CesarClass(string str)
+        {
+            this.Str = str;
+            CesarCode = CreateCesarCode();
+        }
+
+        private int func(char[] text, string text1)
+        {
+            for (int i = 0; i < text1.Length; i++)
+                text[i] = text1[i];
+            return 0;
+        }
+
+        private Dictionary<char, char> CreateCesarCode()
+        {
+            string text1 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+            char[] text = new char[text1.Length];
+            func(text, text1);
+
+            Random random = new Random();
+
+            for (int i = text.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+                char temp = text[j];
+                text[j] = text[i];
+                text[i] = temp;
+            }
+
+            Dictionary<char, char> CesarCode = new Dictionary<char, char>(text1.Length);
+
+            for (int i = 0; i < text1.Length; i++)
+                CesarCode.Add(text1[i], text[i]);
+
+            return CesarCode;
+        }
+
+        private static string CodeCesarCode(string value)
+        {
+            char[] text = new char[value.Length];
+            func(text, value);
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLower(text[i]))
+                    text[i] = CesarCode[text[i]];
+                else
+                {
+                    string temp = Convert.ToString(Char.ToLower(text[i]));
+                    text[i] = Char.ToUpper(CesarCode[temp[0]]);
+                }
+            }
+
+            string text2 = "";
+            foreach (char c in text)
+                text2 += c;
+
+            return text2;
+        }
+
+        static public implicit operator CesarClass(string value)
+        {
+            return CodeCesarCode(value);
+        }
+
+        static public implicit operator string(CesarClass value)
+        {
+            return CodeCesarCode(value.str);
+        }
+    }
+}
---
+++ Lab_3/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Lab_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.MakeCesarCode(richTextBox2);
+        }
+    }
+
+    public static class FormsExtensions
+    {
+
+        public static Dictionary<char, char> CreateCesarCode()
+        {
+            string text1 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+            char[] text = new char[text1.Length];
+            for (int i = 0; i < text1.Length; i++)
+                text[i] = text1[i];
+
+            Random random = new Random();
+
+            for (int i = text.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+                char temp = text[j];
+                text[j] = text[i];
+                text[i] = temp;
+            }
+
+            Dictionary<char, char> CesarCode = new Dictionary<char, char>(text1.Length);
+
+            for (int i = 0; i < text1.Length; i++)
+                CesarCode.Add(text1[i], text[i]);
+
+            return CesarCode;
+        }
+
+        public static Dictionary<char, char> CesarCode = CreateCesarCode();
+
+        public static void MakeCesarCode(this RichTextBox richTextBox1, RichTextBox richTextBox2)
+        {
+            string text1 = richTextBox1.Text;
+
+            char[] text = new char[text1.Length];
+            for (int i = 0; i < text1.Length; i++)
+                text[i] = text1[i];
+
+            
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLower(text[i]))
+                    text[i] = CesarCode[text[i]];
+                else
+                {
+                    string temp = Convert.ToString(Char.ToLower(text[i]));
+                    text[i] = Char.ToUpper(CesarCode[temp[0]]);
+                }
+            }
+
+            string text2 = "";
+            foreach (char c in text)
+                text2 += c;
+
+            richTextBox2.Text = text2;
+        }
+    }
+}
